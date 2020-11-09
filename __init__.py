103872ff9a44a8bfe1cfb72b0fbfed485702bdc5
DaniloToroL <danilo.toro@usach.cl>
2020-11-09 16:40:42 -0300

    first commit

---
+++ __init__.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+"""
+Base para desarrollo de modulos externos.
+Para obtener el modulo/Funcion que se esta llamando:
+     GetParams("module")
+
+Para obtener las variables enviadas desde formulario/comando Rocketbot:
+    var = GetParams(variable)
+    Las "variable" se define en forms del archivo package.json
+
+Para modificar la variable de Rocketbot:
+    SetVar(Variable_Rocketbot, "dato")
+
+Para obtener una variable de Rocketbot:
+    var = GetVar(Variable_Rocketbot)
+
+Para obtener la Opcion seleccionada:
+    opcion = GetParams("option")
+
+
+Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
+    
+   sudo pip install <package> -t .
+
+"""
+
+base_path = tmp_global_obj["basepath"]
+cur_path = base_path + 'modules' + os.sep + 'Browser' + os.sep + 'libs' + os.sep
+sys.path.append(cur_path)
+import websockets
+
+global websockets
+
+module = GetParams("module")
+
+if module == "click":
+    import asyncio
+
+    async def hello():
+        uri = "ws://localhost:8000"
+        async with websockets.connect(uri) as websocket:
+            name = input("What's your name? ")
+
+            await websocket.send(name)
+            print(f"> {name}")
+
+            greeting = await websocket.recv()
+            print(f"< {greeting}")
+
+
+    asyncio.get_event_loop().run_until_complete(hello())
+
+
