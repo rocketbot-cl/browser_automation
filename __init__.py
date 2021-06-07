0d4977af6b9874156b6b1a71c7eb67e25968d7bd
DaniloToroL <danilo.toro@usach.cl>
2021-06-07 10:00:23 -0400

    [feat] support for multiple OS

--- __init__.py
+++ __init__.py
@@ -24,38 +24,99 @@ Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
 
 """
 
-base_path = tmp_global_obj["basepath"]
-cur_path = base_path + 'modules' + os.sep + 'browser_automation' + os.sep + 'libs' + os.sep
-sys.path.append(cur_path)
-
+import sys
+import os
 from selenium.webdriver import Chrome
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
-from time import sleep
-global browser_driver
+import platform
 
+BASE_PATH = tmp_global_obj["basepath"] # Rocketbot path
+cur_path = BASE_PATH + 'modules' + os.sep + 'browser_automation' + os.sep + 'libs' + os.sep
+if cur_path not in sys.path:
+    sys.path.append(cur_path)
+
+
+systems = {
+    'Linux': "linux", 
+    'Darwin': "mac",
+    'Windows': "win"
+}
+SYSTEM = platform.system()
+
+web = GetGlobals('web')
 module = GetParams("module")
+class BrowserAutomation:
+    global BASE_PATH, systems, SYSTEM
 
+    DRIVERS = {
+        "chrome": "chromedriver",
+        "firefox": "x64" + os.sep + "geckodriver"
+    }
+
+    BROWSER_PATHS = {
+        "chrome": {
+            "Windows": r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+            "Linux": "/usr/bin/google-chrome",
+            "Darwin": "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+        }
+    }
+
+    def __init__(self, browser="chrome", driver_path=None, browser_path=""):
+        self.driver_path = driver_path
+        self.browser = browser
+        self.browser_path = browser_path
+        self.port = "5005"
+        self.profile_path = os.path.join(BASE_PATH,'modules','browser_automation','profile')
+
+    @property
+    def driver_path(self):
+        if self.__driver_path:
+            return self.__driver_path
+
+        driver_name = self.DRIVERS[self.browser] + (".exe" if SYSTEM == "Windows" else "")
+        return os.path.join(BASE_PATH, "drivers", systems[SYSTEM], self.browser, driver_name)
+
+    @driver_path.setter
+    def driver_path(self, path):
+        self.__driver_path = path
+
+    @property
+    def browser_path(self):
+        if self.__browser_path:
+            return self.__browser_path
+        return self.BROWSER_PATHS[self.browser][SYSTEM]
+
+    @browser_path.setter
+    def browser_path(self, path):
+        self.__browser_path = path
+
+    def launch_browser(self):
+        import subprocess
+        subprocess.Popen([self.browser_path, "--remote-debugging-port="+self.port, "--user-data-dir=" + self.profile_path])
+    
+    def open(self):
+        global Options, Chrome
+        self.launch_browser()
+        if self.browser == "chrome":
+            chrome_options = Options()
+            chrome_options.debugger_address = "127.0.0.1:" + self.port
+            self.driver = Chrome(chrome_options=chrome_options, executable_path=self.driver_path)
+            return self.driver
+        
 
 if module == "openBrowser":
-    import subprocess
-    web = GetGlobals('web')
 
     url = GetParams("url")
     path = GetParams("path")
-    try:
-        import subprocess
+    browser = GetParams("browser")
 
-        platform_ = platform.system()
-        profile = base_path + 'modules' + os.sep + 'browser_automation' + os.sep + 'profile' + os.sep
-        res = subprocess.Popen(f"{path} --remote-debugging-port=5005 --user-data-dir={profile}")
-        chrome_options = Options()
-        chrome_options.debugger_address = "127.0.0.1:5005"
-        if platform_.endswith('dows'):
-            chrome_driver = os.path.join(base_path, os.path.normpath(r"drivers\win\chrome"), "chromedriver.exe")
-        else:
-            chrome_driver = os.path.join(base_path, os.path.normpath(r"drivers/mac/chrome"), "chromedriver")
-        browser_driver = Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    try:
+        if path:
+            browser = "chrome"
+
+        browser_automation = BrowserAutomation(browser, browser_path=path)
+        browser_driver = browser_automation.open()
 
         web.driver_list[web.driver_actual_id] = browser_driver
         if url:
@@ -67,5 +128,6 @@ if module == "openBrowser":
         raise e
 
 if module == "closeBrowser":
+    browser_driver = web.driver_list[web.driver_actual_id]
     browser_driver.close()
     browser_driver.quit()
