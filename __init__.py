ad418f1de101e0fa2616598aa85d374049f2521d
Guiredys <guiredys.gomez@rocketbot.com>
2025-01-07 17:27:43 -0400

    fix 3.0.1 cerrar multiples pestañas

--- __init__.py
+++ __init__.py
@@ -207,5 +207,8 @@ if module == "openBrowser":
 
 if module == "closeBrowser":
     browser_driver = web.driver_list[web.driver_actual_id]
-    browser_driver.close()
+    browser_windows = browser_driver.window_handles
+    for window in browser_windows:
+        browser_driver.switch_to.window(window)
+        browser_driver.close()
     browser_driver.quit()
